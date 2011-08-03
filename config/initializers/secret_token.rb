38c2a4faaca5c34e6450172d6ef48c42c5d9efc5
Leandro Alvares da Costa <leandroadacosta@MacBook-Air-de-Leandro.local>
2011-08-03 00:50:25 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Poc::Application.config.secret_token = 'd940b62b3c7bb8c85fd0711e47f27864a711613e83185a6ee8f4e149ff78062c7075dd7edb8b3a239d0640ff15a0317ef1f8833640f81f4496e54513f9b32116'
